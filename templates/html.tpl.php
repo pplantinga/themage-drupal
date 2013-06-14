45b99f1a1a7d87af69906d4ecc4ace9efd75c3f3
Peter Massey-Plantinga <plantinga.peter@gmail.com>
2013-06-14 11:31:32 -0700

    Work around skip-link bug in browsers

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -15,7 +15,10 @@
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <nav id="skip-link" role="navigation">
-    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+    <a href="#main-content" class="element-invisible element-focusable"
+      onclick="jQuery('#main-content').attr('tabIndex',-1).focus();">
+        <?php print t('Skip to main content'); ?>
+    </a>
   </nav>
   <?php print $page_top; ?>
   <?php print $page; ?>
